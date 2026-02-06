4cae9fb52e94da11b9ea834825074ae96ef00309
Ryvione <nexusraven4545@gmail.com>
2026-02-06 19:08:47 +0100

    Added ChunkEraser textures, block and added raw ores to World forge

--- Common/src/main/java/com/ryvione/gatheringchunks/common/menus/WorldForgeMenu.java
+++ Common/src/main/java/com/ryvione/gatheringchunks/common/menus/WorldForgeMenu.java
@@ -18,18 +18,27 @@ import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.ContainerData;
 import net.minecraft.world.inventory.SimpleContainerData;
+import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
 public class WorldForgeMenu extends BaseInventoryContainerMenu {
     private final ContainerData containerData;
+    private final Container blockContainer;
+
     public WorldForgeMenu(int menuId, Inventory inventory) {
         this(menuId, inventory, new SimpleContainer(WorldForgeBlockEntity.NUM_ITEM_SLOTS), new SimpleContainerData(WorldForgeBlockEntity.NUM_DATA_ITEMS));
     }
     public WorldForgeMenu(int menuId, Inventory inventory, Container container, ContainerData containerData) {
         super(Services.PLATFORM.worldForgeMenu(), menuId, container, inventory, 8, 84);
         this.containerData = containerData;
+        this.blockContainer = container;
         Preconditions.checkArgument(container.getContainerSize() >= WorldForgeBlockEntity.NUM_ITEM_SLOTS, "Expected " + WorldForgeBlockEntity.NUM_ITEM_SLOTS + " item slots, but entity has " + container.getContainerSize());
         Preconditions.checkArgument(containerData.getCount() >= WorldForgeBlockEntity.NUM_DATA_ITEMS, "Expected " + WorldForgeBlockEntity.NUM_DATA_ITEMS + " data items, but entity has " + containerData.getCount());
-        addSlot(new FilteredSlot(container, WorldForgeBlockEntity.SLOT_INPUT, 58, 35, WorldForgeBlockEntity::isWorldForgeFuel));
+        addSlot(new Slot(container, WorldForgeBlockEntity.SLOT_INPUT, 58, 35) {
+            @Override
+            public boolean mayPlace(ItemStack stack) {
+                return blockContainer.canPlaceItem(WorldForgeBlockEntity.SLOT_INPUT, stack);
+            }
+        });
         addSlot(new TakeOnlySlot(container, WorldForgeBlockEntity.SLOT_RESULT, 116, 35));
         addDataSlots(containerData);
     }
@@ -41,7 +50,7 @@ public class WorldForgeMenu extends BaseInventoryContainerMenu {
     }
     @Override
     protected boolean quickMoveToContainer(ItemStack stack) {
-        if (WorldForgeBlockEntity.isWorldForgeFuel(stack)) {
+        if (this.blockContainer.canPlaceItem(WorldForgeBlockEntity.SLOT_INPUT, stack)) {
             return this.moveItemStackToContainerSlot(stack, WorldForgeBlockEntity.SLOT_INPUT, WorldForgeBlockEntity.SLOT_INPUT + 1, false);
         }
         return false;
