1143f230cf00ea687a803e70bdcb59e8db853305
Ryvione <nexusraven4545@gmail.com>
2026-02-06 22:02:40 +0100

    Fixed Chunk Eraser & Engine textures (yes again this annoying) and changed UpdateNotificationOverlay

--- Common/src/main/java/com/ryvione/gatheringchunks/client/UpdateNotificationOverlay.java
+++ Common/src/main/java/com/ryvione/gatheringchunks/client/UpdateNotificationOverlay.java
@@ -9,7 +9,6 @@ import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraft.network.chat.Component;
 
 public class UpdateNotificationOverlay {
-
     public static void renderUpdateWarning(GuiGraphics guiGraphics, TitleScreen screen) {
         if (!UpdateChecker.isUpdateAvailable()) {
             return;
@@ -23,21 +22,16 @@ public class UpdateNotificationOverlay {
         Minecraft minecraft = Minecraft.getInstance();
         int screenWidth = screen.width;
         int screenHeight = screen.height;
-
         int warningY = screenHeight - 30;
 
-        String warningText = "Update Available: " + versionInfo.getVersion() +
-                (versionInfo.isBeta() ? " (Beta)" : "");
+        String warningText = "Update Available: " + UpdateChecker.getFormattedLatestVersion();
         Component warningComponent = Component.literal(warningText);
-
         int textWidth = minecraft.font.width(warningComponent);
         int warningX = (screenWidth - textWidth) / 2;
 
         RenderSystem.enableBlend();
         guiGraphics.fill(warningX - 5, warningY - 2, warningX + textWidth + 5, warningY + 10, 0x88000000);
-
         guiGraphics.drawCenteredString(minecraft.font, warningComponent, screenWidth / 2, warningY, 0xFFFF55);
-
         RenderSystem.disableBlend();
     }
 }
